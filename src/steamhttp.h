6acd63265292faa41cdb8dcca95c672d94f94eb4
Tim Schumacher <timschumi@gmx.de>
2019-12-27 18:08:58 +0100

    Remove unused version variable
    We don't have a default User-Agent anymore, so we'll just drop it.

--- src/steamhttp.h
+++ src/steamhttp.h
@@ -1,8 +1,6 @@
 #include "GarrysMod/Lua/Interface.h"
 #include "http.h"
 
-#define STEAMHTTP_VERSION "0.0"
-
 bool processRequest(HTTPRequest request);
 void runFailedHandler(GarrysMod::Lua::ILuaBase *LUA, int handler, std::string reason);
 void runSuccessHandler(GarrysMod::Lua::ILuaBase *LUA, int handler, HTTPResponse response);
